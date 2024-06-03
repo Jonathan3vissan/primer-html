b2716355db51a1342ae46d9a9b428f5e36262883
Jonnthan3visan <joptrabajo@hotmail.com>
2024-06-03 12:50:01 -0300

    revisar porque no salen para elegir inciar sesion o registrarse en index

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-const leer =require ("prompt-sync")();
+const leer = require("prompt-sync")();
 
 
 
-function main() {
-    
-console.log("hola");
+function autetificaion() {
+    let elemento_1 = 0;
+    let elemento_2 = 0;
+    let elemento_3 = 0;
+    let elemento_4 = 0;
+    let condicion_1 = false;
+    let condicion_2 = false;
+
+    elemento_1 = document.getElementById("usuario_registrado")
+    elemento_2 = document.getElementById("pasword_registrado")
+    elemento_3 = document.getElementById("inicia_sesion_nombre")
+    elemento_4 = document.getElementById("inicia_sesion_pasword")
+
+    condicion_1 = elemento_1 === elemento_3
+    condicion_2 = elemento_2 === elemento_4
+
+    if ((!condicion_1) && (!condicion_2)) {
+        console.log("verifque sus datos");
+    } else {
+        console.log("bienvenido", elemento_1);
+    }
+
+
+
 
 
 }
 
 
-main();
+autetificaion();

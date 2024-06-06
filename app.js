53ac7abe6cadf6e5f24ea659e6213685be262440
-1 <>
2024-06-06 10:48:27 -0300

    programandop el inicio de sesion

--- app.js
+++ app.js
@@ -4,8 +4,17 @@
 function recibir_informacion() {
     
     
-
-
+    const nombre_usuario = document.querySelector("#nombre_usuario").value
+    const contrasenia_usuario = document.querySelector("#contrasenia_usuario").value
+    console.log(nombre_usuario);
+    console.log(contrasenia_usuario);
+    localStorage.setItem("nombre-usuario",JSON.stringify(nombre_usuario))
+    localStorage.setItem("nombre-contrasenia",JSON.stringify(contrasenia_usuario))
+    console.log( JSON.parse(localStorage.getItem("nombre-usuario")));
+    console.log( JSON.parse(localStorage.getItem("contrasenia-usuario")));
+let condicion_reciubida=JSON.parse(localStorage.getItem("nombre-usuario"))
+    if(condicion_reciubida===)
+    
 
 }
 
